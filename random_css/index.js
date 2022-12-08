24216180ec0ae12424f3fe41b3feeef8ce8783fb
hrim <hrim.wk@gmail.com>
2022-12-08 15:28:02 +0900

    [Delete] console.log

--- random_css/index.js
+++ random_css/index.js
@@ -15,20 +15,15 @@ button.addEventListener('click', (event) => {
     });
   }
 
-  console.log('버튼누르고맵', textList);
   text.innerHTML = textList.join('');
   if (textList.length > 1) {
-    //난수의 갯수 정하기
     let randomi = Math.floor(Math.random() * (textList.length - 1));
-    console.log('randomi', randomi);
 
-    //그 수만큼 난수 만들기
     for (let i = 0; i < randomi; i++) {
       const num = Math.floor(Math.random() * (textList.length - 1 + 1)) + 1;
 
       randomArr.push(num);
     }
-    console.log('배열', randomArr);
     randomArr.map((data) => {
       document.querySelectorAll('span')[data - 1].classList.add('random');
     });
